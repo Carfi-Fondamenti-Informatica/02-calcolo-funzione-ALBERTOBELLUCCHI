a2d32d0d4589cf95912f87e20650f33113d926af
Alberto_Bellucchi <115469272+ALBERTOBELLUCCHI@users.noreply.github.com>
2022-11-14 14:11:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main() {
-   return 0;
+    float a, b, x, y;
+    cin>>a>>b>>x>>y;//input
+
+    if( x<0 && y>0) cout<<(a*x)-(b*y)<<endl;//f1
+    else if( x>=0 && y<=0) cout<< (a*pow(x,2))-(b*y)<<endl;//f2
+    else cout<<(a*x) + (b*pow(y,2))<<endl;//f3
+
+    return 0;
 }
